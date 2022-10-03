0a4d11cbec4c2e0296651d293b0363cc4c17b788
zhenyaStory <zh.dubinkin@gmail.com>
2022-10-03 22:49:56 +0300

    fix site

--- gulpfile.js
+++ gulpfile.js
@@ -13,7 +13,7 @@ let path = {
     },
     src: {
         html: [source_folder + "/*.html", "!" + source_folder + "/_*.html"],
-        css: source_folder + "/scss/style.scss",
+        css: source_folder + "/scss/stylee.scss",
         js: source_folder + "/js/script.js",
         img: source_folder + "/img/**/*.{jpg,png,svg,gif,ico,webp}",
         fonts: source_folder + "/fonts/*.ttf", 
